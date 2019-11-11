dcad5e74ddb755bcb6a24cac8357abc9204b4847
forusers12 <forusers12@gmail.com>
2019-11-10 21:21:41 -0500

    [-ing] sum_matrix

--- Xinterval_numberN.cpp
+++ Xinterval_numberN.cpp
@@ -1,4 +1,6 @@
-// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를
+// n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, 
+// solution을 완성해주세요.
 
 #include <iostream>
 #include <string>
@@ -8,30 +10,22 @@ using namespace std;
 
 vector<long long> solution(int x, int n) {
     vector<long long> answer;
-    //vector<long long>::iterator it;
     
-
-    
-    answer.push_back(1);
-    answer.push_back(3);
-    //it=answer.begin();
-    // it++;
-    answer.erase(answer.end()-1);
-    
+    for(int i = 1; i <= n; i++)
+        answer.push_back(x*i);
 
     return answer;
 }
 
 int main()
 {
-    vector<long long> abc = solution(2,4);
+    vector<long long> abc;
+    int x,n;
+    cin >> x >> n;
+    abc = solution(x,n);
 
     for(int i = 0; i < abc.size(); i++)
-    {
-        cout << abc[i] << '\n';
-    }
-    
-    
+        cout << abc[i];
 
     return 0;
 }
