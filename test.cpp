b5eb3b576a2f7d4c45965570648ab539c749f395
forusers12 <forusers12@gmail.com>
2020-01-06 18:24:16 +0900

    The one makes everything change to everything.

--- test.cpp
+++ test.cpp
@@ -1,45 +1,22 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
-int stack[10001], top = -1;
-void push(int x){
-    stack[++top] = x;
-}
-int empty() {
-    if (top < 0)return 1;
-    else return 0;
-}
-void pop() {
-    if (empty() == 1)cout << "-1"<<"\n";
-    else {
-        cout << stack[top] << "\n";
-        stack[top--] = 0;
-    }
-}
- 
-int main() {
+
+
+int main()
+{
     int n;
     cin >> n;
+
     for (int i = 0; i < n; i++) {
-        char str[10];
-        cin >> str;
-        if (!strcmp(str, "push")) {
-            int x; cin >> x;
-            push(x);
+        string str; cin >> str;
+        int count = 0;
+
+        for (int j = 0; j < str.size() && count >= 0; j++) {
+            if (str[j] == '(') count++;
+            else count--;
         }
-        else if (!strcmp(str, "top")) {//top()함수를 만들면 top 변수와 겹치기 때문에 만들지 않음
-            if (empty() == 1)cout << "-1"<<"\n";
-            else cout << stack[top] << "\n";
-        }
-        else if (!strcmp(str, "pop")) {
-            pop();
-        }
-        else if (!strcmp(str, "empty")) {
-            cout << empty() << "\n";
-        }
-        else {//empty()함수는 간단하기 때문에 함수를 만들지 않음
-            cout << top + 1 << "\n";
-        }
+
+        if (count == 0) cout << "YES" << '\n';
+        else cout << "NO" << '\n';
     }
-    return 0;
 }
